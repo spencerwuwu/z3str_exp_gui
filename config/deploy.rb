3d15b9857219f5f9d3283738330b2f5c519862f4
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-03-19 16:56:43 +0800

    Add master key with linkdir

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :puma_worker_timeout, nil
+set :linked_files, %w{config/master.key}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
